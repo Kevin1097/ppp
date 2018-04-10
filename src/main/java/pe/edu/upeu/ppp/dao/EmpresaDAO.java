61838f9a48bc4052eb2f1e1f85d11a0e75c3b993
kevin1097 <kevinmogollon@upeu.edu.pe>
2018-04-10 01:28:54 +0000

    index modifica

---
+++ src/main/java/pe/edu/upeu/ppp/dao/EmpresaDAO.java
@@ -0,0 +1,31 @@
+ckage pe.edu.upeu.ppp.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface EmpresaDAO {
+
+	// Este meto usa el procedimiento PA_REG_EMPRESA_P1_AND_P2
+	 int RegEmpresa(String P_NOMBRE, 
+			 	  String P_APELLIDOS,
+			 	  String P_DNI,
+			 	  String P_CELULAR,
+			 	  String P_CORREO,
+			 	  String P_GENERO,
+			 	  
+			 	  String P_CARGO,
+			 	  String P_IDPERIODO,
+			 	  int p_idalumno,
+			 	 
+			 	  String P_RAZONSOCIAL,
+			 	  String P_RUC,
+			 	  String P_DIRECCION,
+			 	  String P_ACTIVIDAD,
+			 	  int p_IdEstado);
+
+	 //Listar seguros de empresas
+	 List<Map<String, Object>> ListSeguro();
+	 
+	 //LISTAMOS LINEAS ACADEMICAS
+	 List<Map<String, Object>> ListLineas();
+}
